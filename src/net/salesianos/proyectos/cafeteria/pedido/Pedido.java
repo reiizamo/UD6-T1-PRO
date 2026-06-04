49830ba1760e1343c82163f38b59e15a53133dea
Asier Zamora <asierferzam@gmail.com>
2026-06-04 18:49:05 +0100

    Atributos y constructores

--- src/net/salesianos/proyectos/cafeteria/pedido/Pedido.java
+++ src/net/salesianos/proyectos/cafeteria/pedido/Pedido.java
@@ -1,5 +1,17 @@
 package net.salesianos.proyectos.cafeteria.pedido;
 
 public class Pedido {
-    
+    private String codigo;
+    private String cliente;
+    private String producto;
+    private boolean paraLlevar;
+
+    public Pedido(){}
+
+    public Pedido(String codigo, String cliente, String producto, boolean paraLlevar){
+        this.codigo = codigo;
+        this.cliente = cliente;
+        this.producto = producto;
+        this.paraLlevar = paraLlevar;
+    }
 }
