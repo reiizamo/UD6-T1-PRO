69199b2dfac97eade2b8026625caf29b139c4eef
Asier Zamora <asierferzam@gmail.com>
2026-06-05 12:59:40 +0100

    Atributos y Constructores

--- src/net/salesianos/proyectos/aplicación/pelicula/Pelicula.java
+++ src/net/salesianos/proyectos/aplicación/pelicula/Pelicula.java
@@ -1,5 +1,15 @@
 package net.salesianos.proyectos.aplicación.pelicula;
 
 public class Pelicula {
-    
+    private String titulo;
+    private String genero;
+    private int duracionMinutos;
+
+    public Pelicula(){}
+
+    public Pelicula(String titulo, String genero, int duracionMinutos){
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracionMinutos = duracionMinutos;
+    }
 }
