a25ca33217ef475a28783a0034cd176d1e8f9678
Asier Zamora <asierferzam@gmail.com>
2026-06-05 13:03:06 +0100

    Override metodo

--- src/net/salesianos/proyectos/aplicación/pelicula/Pelicula.java
+++ src/net/salesianos/proyectos/aplicación/pelicula/Pelicula.java
@@ -33,5 +33,9 @@ public class Pelicula {
         this.duracionMinutos = duracion;
     }
 
+    @Override
+    public String toString() {
+        return this.titulo + " " + this.genero + " " + this.duracionMinutos;
+    }
 
 }
