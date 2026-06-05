7cff67010a39d6369e6d0bbd17305b1ee7dc0545
Asier Zamora <asierferzam@gmail.com>
2026-06-05 08:46:57 +0100

    Pedidos y comanda

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import net.salesianos.proyectos.cafeteria.pedido.Pedido;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pedido p1 = new Pedido("P001", "Carla", "Café con leche", true);
+        Pedido p2 = new Pedido("P002", "Mario", "Bocadillo de pollo", false);
+        Pedido p3 = new Pedido("P003", "Nerea", "Zumo natural", true);
+        Pedido p4 = new Pedido("P004", "Iván", "Tostada integral", false);
+        Pedido p5 = new Pedido("P005", "Laura", "Té verde", true);
+
+        Queue<Pedido> pedidos = new LinkedList<>();
+        pedidos.add(p1);
+        pedidos.add(p2);
+        pedidos.add(p3);
+        pedidos.add(p4);
+        pedidos.add(p5);
+
+        
+
+
+
+
+        
+
+
     }
 }
