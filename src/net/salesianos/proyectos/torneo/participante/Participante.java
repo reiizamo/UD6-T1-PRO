87fa660dfd8a9d40bb30ee0ff6d73bd67aff24b3
Asier Zamora <asierferzam@gmail.com>
2026-06-05 09:08:56 +0100

    Atributos y constructores

--- src/net/salesianos/proyectos/torneo/participante/Participante.java
+++ src/net/salesianos/proyectos/torneo/participante/Participante.java
@@ -1,5 +1,16 @@
 package net.salesianos.proyectos.torneo.participante;
 
 public class Participante {
+    private String idJugador;
+    private String nombre;
+    private String categoria;
+
+    public Participante(){}
+
+    public Participante(String idJugador, String nombre, String categoria){
+        this.idJugador = idJugador;
+        this.nombre = nombre;
+        this.categoria = categoria;
+    }
     
 }
