de051f0672a2196c90be0ae9f0e10b8338d200a3
Asier Zamora <asierferzam@gmail.com>
2026-06-05 13:02:11 +0100

    Getters y Setters

--- src/net/salesianos/proyectos/aplicación/pelicula/Pelicula.java
+++ src/net/salesianos/proyectos/aplicación/pelicula/Pelicula.java
@@ -7,9 +7,31 @@ public class Pelicula {
 
     public Pelicula(){}
 
-    public Pelicula(String titulo, String genero, int duracionMinutos){
+    public Pelicula(String titulo, String genero, int duracion){
         this.titulo = titulo;
         this.genero = genero;
-        this.duracionMinutos = duracionMinutos;
+        this.duracionMinutos = duracion;
     }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+    public int getDuracion(){
+        return this.duracionMinutos;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+    public void setDuracion(int duracion){
+        this.duracionMinutos = duracion;
+    }
+
+
 }
